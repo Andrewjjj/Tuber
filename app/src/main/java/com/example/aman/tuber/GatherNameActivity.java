c9b6ca114f8b95224274377c250ddf6a4594a6ae
adithasan <adit@ualberta.ca>
2019-01-19 15:50:01 -0700

    Added the first onboarding screen

---
+++ app/src/main/java/com/example/aman/tuber/GatherNameActivity.java
@@ -0,0 +1,13 @@
+package com.example.aman.tuber;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GatherNameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gather_name);
+    }
+}
