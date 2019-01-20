89c018191e2bc820d191204367a22e9f2a3623e3
adithasan <adit@ualberta.ca>
2019-01-20 03:20:31 -0700

    Added ids to skills

--- app/src/main/java/com/example/aman/tuber/Skill.java
+++ app/src/main/java/com/example/aman/tuber/Skill.java
@@ -4,18 +4,22 @@ public class Skill
 {
     String mSkillName;
     String mSkillDescription;
+    String mSkillId; // this is UID+SkillName;
 
     public Skill(){}
 
-    public Skill(String skillName, String skillDescription)
+    public Skill(String skillName, String skillDescription, String skillId)
     {
         mSkillName = skillName;
         mSkillDescription = skillDescription;
+        mSkillId = skillId;
     }
 
     public String GetSkillName() { return mSkillName; }
     public String GetSkillDescription() { return mSkillDescription; }
+    public String GetSkillId() { return mSkillId; }
 
     public void SetSkillName(String skillName) { mSkillName = skillName; }
     public void SetSkillDescription(String skillDescription) { mSkillDescription = skillDescription; }
+    public void SetSkillId(String skillId) { mSkillId = skillId; }
 }
