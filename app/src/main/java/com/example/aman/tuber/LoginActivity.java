d2eb197b290b4a14a8132bb31536b7ba8a1cf436
Aman Anand <aanand@ualberta.ca>
2019-01-19 20:02:48 -0700

    checking FCM

--- app/src/main/java/com/example/aman/tuber/LoginActivity.java
+++ app/src/main/java/com/example/aman/tuber/LoginActivity.java
@@ -17,6 +17,8 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.onesignal.OneSignal;
+
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -40,63 +42,17 @@ public class LoginActivity extends AppCompatActivity {
 
         databaseUserProfile = FirebaseDatabase.getInstance().getReference("UserProfiles");
 
+        // TODO: FCM
+        OneSignal.startInit(this)
+                .inFocusDisplaying(OneSignal.OSInFocusDisplayOption.Notification)
+                .unsubscribeWhenNotificationsAreDisabled(true)
+                .init();
+
 
     }
 
     public void SignUp(View view){
 
-        final String email = editTextEmail.getText().toString().trim();
-        final String password  = editTextPassword.getText().toString().trim();
-
-
-        //checking if email and passwords are empty
-        if(TextUtils.isEmpty(email)){
-            Toast.makeText(this,"Please enter email",Toast.LENGTH_LONG).show();
-            return;
-        }
-
-        if(TextUtils.isEmpty(password)){
-            Toast.makeText(this,"Please enter password",Toast.LENGTH_LONG).show();
-            return;
-        }
-
-        //creating a new user
-        firebaseAuth.createUserWithEmailAndPassword(email,password)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if(task.isSuccessful()){
-                            Toast.makeText(LoginActivity.this, "Registered Successfully", Toast.LENGTH_SHORT).show();
-
-                            DataRepositorySingleton singleton = DataRepositorySingleton.GetInstance() ;
-
-                            FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
-                            singleton.SetUser(firebaseUser);
-
-                            UserProfile user = new UserProfile(email);
-                            singleton.setUserProfile(user);
-
-                            // add to the database
-                            addUser(user);
-
-                        }else{
-                            Toast.makeText(LoginActivity.this, "Could Not Register. Please try again.", Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                });
-    }
-
-    private void addUser(UserProfile userProfile){
-
-        //getting a unique id using push().getKey() method
-        //it will create a unique id and we will use it as the Primary Key for our User
-        String id = databaseUserProfile.push().getKey();
-
-        // Saving the UserProfile
-        databaseUserProfile.child(id).setValue(userProfile);
-
-        //displaying a success toast
-        Toast.makeText(this, "added to database", Toast.LENGTH_LONG).show();
 
     }
 
@@ -124,12 +80,7 @@ public class LoginActivity extends AppCompatActivity {
                         if(task.isSuccessful()){
                             Toast.makeText(LoginActivity.this, "Signed In Successfully", Toast.LENGTH_SHORT).show();
 
-                            DataRepositorySingleton singleton = DataRepositorySingleton.GetInstance() ;
-
-                            FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
-                            singleton.SetUser(firebaseUser);
-
-                            singleton.setUserProfile( new UserProfile(email) );
+                            OneSignal.sendTag("User_ID",email);
 
                             // close this activity
 //                            finish();TODO
