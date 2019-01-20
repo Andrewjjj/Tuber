047d814422bb388a5d0a7e39397a64b450ead89f
Andrewjjj <Juwon1@ualberta.ca>
2019-01-19 23:17:56 -0700

    Andrew

--- app/src/main/java/com/example/aman/tuber/PopActivity.java
+++ app/src/main/java/com/example/aman/tuber/PopActivity.java
@@ -18,7 +18,7 @@ public class PopActivity extends Activity {
         int width = dm.widthPixels;
         int height = dm.heightPixels;
 
-        getWindow().setLayout((int)(width*.8), (int)(height*.7));
+        getWindow().setLayout((int)(width*.7), (int)(height*.6));
         WindowManager.LayoutParams params = getWindow().getAttributes();
         params.gravity = Gravity.CENTER;
         params.x = 0;
